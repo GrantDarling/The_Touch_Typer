12c3c867439bfb797ec2986cffc858e6bf6ef73c
Grant Darling <grantcarterdarling@gmail.com>
2020-11-14 12:33:15 -0500

    created a working register and login page

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,52 @@
+import React, { Fragment, useState } from 'react';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('SUCCESS');
+  };
+
+  return (
+    <Fragment>
+      <h1>Sign-In</h1>
+      <p>Sign Into Your Account</p>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <button type='submit' value='login'>
+          Login
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,79 @@
+import React, { Fragment, useState } from 'react';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log('SUCCESS');
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1>Sign-Up</h1>
+      <p>Create Your Account</p>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <div>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            value={password2}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <button type='submit' value='register'>
+          Register
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Register;
