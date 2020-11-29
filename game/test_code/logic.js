84b979ba92517755884bb6ff38be4324ab4fe9f9
Grant Darling <grantcarterdarling@gmail.com>
2020-11-28 23:19:22 -0500

    Finished basic logic for game

--- game/test_code/logic.js
+++ game/test_code/logic.js
@@ -1,5 +1,5 @@
 const display = [
-  'ffj jff j f jfj fj j ff jjj fjfj jjj f ffj jffj jff fj jf jfff jfjf jfj fj j f f jjf ',
+  'ffj jff j f jfj fj j ff jjj fjfj jjj f ffj jffj jff fj jf jfff jfjf jfj fj j f f jjf',
 ];
 
 const drawDisplay = () => {
@@ -14,7 +14,7 @@ const drawDisplay = () => {
     if (word !== ' ') {
       wordContainer.classList.add('word');
     } else {
-      wordContainer.classList.add('space');
+      wordContainer.classList.add('word-space');
       wordContainer.innerHTML = '';
     }
 
@@ -41,11 +41,13 @@ const drawDisplay = () => {
   let advancer = 0;
   let startScreen = document.querySelector('#start-screen');
   let started = false;
+  let startTime;
   let ended = false;
   let initialized = false;
   let totalAttempts = -1;
   let totalSuccesses = 0;
   let milliseconds = 0;
+  let totalElements;
 
   // Increase Attempts & Successes
   let increaseAttempts = () => totalAttempts++;
@@ -80,40 +82,90 @@ const drawDisplay = () => {
     }, timeout);
 
   // Check current word !!!
-  let getCurrentWordIndex = () => {
+  let selectActiveWord = () => {
+    let lastWordSelector;
+    if (advancer >= 1) {
+      let lastWord =
+        '#' +
+        document.getElementsByClassName('letter')[advancer - 1].parentNode.id;
+      lastWordSelector = document.querySelector(lastWord);
+      lastWordSelector.classList.remove('word--active');
+    }
+    let currentWord =
+      '#' + document.getElementsByClassName('letter')[advancer].parentNode.id;
+    let wordSelector = document.querySelector(currentWord);
+    wordSelector.classList.add('word--active');
+    // !!! Move this
+    console.log(wordSelector.children[0].classList.contains('space'));
+    if (
+      wordSelector !== lastWordSelector &&
+      wordSelector.children[0].classList.contains('space')
+    ) {
+      displayWPM();
+    }
+
     // var li = document.getElementById('myId');
     // alert(li.className);
   };
 
+  // Grab current time
+  let getTimeElapsed = () => {
+    let currentTime = Date.now() / 60000;
+    let timeElapsed = currentTime - startTime;
+    console.log(timeElapsed.toFixed(2));
+    return timeElapsed.toFixed(2);
+  };
+
+  let getCurrentWordPosition = () => {
+    let currentWord = document.getElementsByClassName('letter')[advancer]
+      .parentNode.id;
+    let currentPosition = currentWord.split('word-').slice(0).join('');
+    console.log(currentPosition);
+    return currentPosition;
+  };
+
   // calculate words per minute !!!
   let calculateWPM = () => {
-    // take current (word - 1) and divide by total minutes
-    // getCurrentWordIndex - millisecondsToMinutes
+    let currentTime = getTimeElapsed();
+    let totalWords = getCurrentWordPosition() / 2; // Divide to compensate for spaces
+
+    let WPM = (totalWords / currentTime).toFixed(1);
+    console.log('WPM: ' + WPM);
+    return WPM;
   };
 
   // display current WPM !!!
   let displayWPM = () => {
-    // make inner html the calculated WPM
-  };
-
-  // Figure out when the game finishes
-  let gameOver = () => {
-    // if game is over then gameOver = true;
+    const wpmDisplay = document.querySelector('#wpm');
+    let WPM = calculateWPM();
+    isNaN(WPM)
+      ? (wpmDisplay.innerHTML = 'WPM: 0')
+      : (wpmDisplay.innerHTML = 'WPM: ' + WPM);
   };
 
   // Begin game
   let startGame = (e) => {
     if (e.key === ' ' && initialized === false) {
+      let totalWords = document.querySelectorAll('.word').length;
+      let totalSpaces = document.querySelectorAll('.space').length;
+      let totalElements = totalWords + totalSpaces;
       initialized = true;
       started = true;
       startScreen.style.display = 'none';
-      startTimer(5000);
+      startTime = Date.now() / 60000;
+      console.log(
+        'total elements to countdown (startGame Func)' + totalElements
+      );
+      return totalElements;
+      //startTimer(5000);
     }
     drawCursor();
   };
 
   // End game
-  let endGame = () => {};
+  let endGame = () => {
+    alert('game is over!!!');
+  };
 
   // Draw Cursor
   let drawCursor = () => {
@@ -155,6 +207,17 @@ const drawDisplay = () => {
       increaseAttempts();
       checkKey(e);
       accuracyDisplayer();
+      selectActiveWord();
+    }
+
+    let currentPosition = getCurrentWordPosition();
+    let totalWords = document.querySelectorAll('.word').length;
+    let totalSpaces = document.querySelectorAll('.space').length;
+    let totalElements = totalWords + totalSpaces;
+
+    console.log('POS: ' + currentPosition + 'TOTAL: ' + (totalElements - 2));
+    if (currentPosition >= totalElements - 1) {
+      endGame();
     }
   };
 
